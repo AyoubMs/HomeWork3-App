78737d862ae76a1d523c66bc7d14493b9bc591d7
AyoubMS <chahayb563@gmail.com>
2023-07-05 13:05:54 +0200

    add app

---
+++ Homework3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+Console.Write("Enter your first name: ");
+
+string? firstName = Console.ReadLine();
+
+if (firstName.ToLower().Equals("tim") || firstName.ToLower().Equals("timothey"))
+{
+    Console.WriteLine("Welcome Timothey!");
+}
+else
+{
+    Console.WriteLine($"Welcome {firstName.ToLower()}");
+}
+
+
